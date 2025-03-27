40be21c0bf58a6aacc6482b6fe167ca676772952
Jaime5913 <98013443+Jaime5913@users.noreply.github.com>
2025-03-27 13:15:16 -0600

    rework

--- securingweb/src/test/java/com/example/securingweb/SecuringwebApplicationTests.java
+++ securingweb/src/test/java/com/example/securingweb/SecuringwebApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.securingweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SecuringwebApplicationTests {
 
 	@Test
 	void contextLoads() {
